11d5321064d0baee086eb1d9c7d75f7b211aebaf
mayank44287 <mayank44287@gmail.com>
2023-06-27 21:23:36 -0700

    added predicate,transfor traits for map, flatmap and filter methods through anonymous classes

--- src/main/scala/exercises/MyList.scala
+++ src/main/scala/exercises/MyList.scala
@@ -1,5 +1,6 @@
 package exercises
 import java.util.*
+import scala.runtime.Nothing$
 
 abstract class MyList[+A] {
 
@@ -20,6 +21,16 @@ abstract class MyList[+A] {
 
   override def toString: String = "[" + printElements + "]"
 
+  def map[B](transformer: MyTransformer[A, B]): MyList[B]
+
+  def flatMap[B](transformer: MyTransformer[A, MyList[B]]): MyList[B]
+
+  def filter(predicate: MyPredicate[A]): MyList[A]
+
+  //concatenation method for the purpose of flatmap method
+  def ++[B >: A](list: MyList[B]): MyList[B]
+
+
 }
 
 object Empty extends MyList[Nothing] {
@@ -32,6 +43,14 @@ object Empty extends MyList[Nothing] {
   def add[B >: Nothing](element: B): MyList[B] = new Cons(element, Empty)
 
   override def printElements: String = ""
+
+  def map[B](transformer: MyTransformer[Nothing, B]): MyList[B] = Empty
+
+  def flatMap[B](transformer: MyTransformer[Nothing, MyList[B]]): MyList[B] = Empty
+
+  def filter(predicate: MyPredicate[Nothing]): MyList[Nothing] = Empty
+
+  def ++[B >: Nothing](list: MyList[B]): MyList[B] = list
 }
 
 class Cons[+A](h: A, t: MyList[A]) extends MyList[A]{
@@ -47,6 +66,53 @@ class Cons[+A](h: A, t: MyList[A]) extends MyList[A]{
     if (t.isEmpty) "" + h
     else s"$h ${t.printElements}"
   }
+
+  /*  [1,2,3].map(n*2)
+  *  = new Cons(2, [2,3].map(n*2))
+  *  = new Cons(2, new Cons(4, [3].map(n*2)))
+  *  = new Cons(2, new Cons(4, new Cons(6, Empty.map(n*2))))
+  *  = new Cons(2, new Cons(4, new Cons(6, Empty)))
+  * */
+  def map[B](transformer: MyTransformer[A, B]): MyList[B] = {
+    new Cons(transformer.transform(h), t.map(transformer))
+  }
+
+  /*  [1,2].flatMap(n => [n, n+1])
+  * = [1,2] ++ [2].flatMap(n => [n, n+1])
+  * = [1,2] ++ [2,3] ++ Empty.flatMap(n => [n, n+1])
+  * = [1,2] ++ [2,3] ++ Empty
+  * = [1,2,2,3]
+  * */
+  def flatMap[B](transformer: MyTransformer[A, MyList[B]]): MyList[B] = {
+
+    transformer.transform(h) ++ t.flatMap(transformer)
+  }
+
+  /* [1,2,3].filter(n % 2 == 0)
+  *  = [2,3].filter(n % 2 == 0)
+  *  = new Cons(2, [3].filter(n % 2 == 0)
+  *  = new Cons(2, Empty.filter( n % 2 == 0)
+  *  = new Cons(2, Empty)
+  * */
+  def filter(predicate: MyPredicate[A]): MyList[A] = {
+    if (predicate.test(h)) new Cons(h, t.filter(predicate))
+    else t.filter(predicate)
+  }
+
+  /* [1,2] ++ [3,4,5]
+  *  = new Cons(1, [2] ++ [3,4,5])
+  *  = new Cons(1, new Cons(2, Empty ++ [3,4,5])
+  *  = new Cons(1, new Cons(2, new Cons(3, new Cons(4, new Cons(5))))
+  * */
+  def ++[B >: A](list: MyList[B]): MyList[B] = new Cons(h, t ++ list)
+}
+
+trait MyPredicate[-T] {
+  def test(elem: T): Boolean
+}
+
+trait MyTransformer[-A,B]{
+  def transform(elem: A): B
 }
 
 object ListTest extends App{
@@ -60,7 +126,27 @@ object ListTest extends App{
   //after adding generic class
   val listOfIntegers : MyList[Int] = new Cons(1, new Cons(2, new Cons(3, Empty)))
   val listOfStrings : MyList[String] = new Cons("Hello", new Cons("Mayank", Empty))
+  val anotherListOfIntegers: MyList[Int] = new Cons(4, new Cons(5, Empty))
 
   println(listOfIntegers)
-  print(listOfStrings)
+  println(listOfStrings)
+
+  println(listOfIntegers.map(new MyTransformer[Int, Int] {
+    override def transform(elem: Int): Int = elem * 2
+  }).toString)
+
+  println(listOfIntegers.filter(new MyPredicate[Int] {
+    override def test(elem: Int): Boolean = elem % 2 == 0
+  }).toString)
+
+  //concatentation method
+  println(listOfIntegers ++ anotherListOfIntegers)
+
+  println(listOfIntegers.flatMap(new MyTransformer[Int, MyList[Int]] {
+    override def transform(elem: Int): MyList[Int] = new Cons(elem, new Cons(elem + 1, Empty))
+  }).toString)
+
+
+
+
 }
